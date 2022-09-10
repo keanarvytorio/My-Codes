b8b5c16e1f4f9528ac6efc43fd15798b677c257b
Kean Arvy <toriokean27@gmail.com>
2022-09-10 15:51:56 +0800

    added all

--- exceptions.py
+++ exceptions.py
@@ -1,9 +1,17 @@
 from tkinter import Y
+import sys
 
+try:    
+    x = int(input("x: "))
+    y = int(input("y: "))
+except ValueError:
+    print("Error: Invalid input.")
+    sys.exit(1)
 
-x = int(input("x: "))
-y = int(input("y: "))
-
-result = x / y
+try:
+    result = x / y
+except ZeroDivisionError:
+    print("Error: Cannot divide by 0.")
+    sys.exit(1)
 
 print(f"{x} / {y} = {result}")
